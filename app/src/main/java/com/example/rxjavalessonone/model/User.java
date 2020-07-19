d47fb306cbc43574c104d6a80398aa6456fb7b78
Сафаров Эльшад Нурали оглы <elshad.safarov@domru.ru>
2020-07-19 23:44:35 +0500

    Create IRxUtils

---
+++ app/src/main/java/com/example/rxjavalessonone/model/User.java
@@ -0,0 +1,4 @@
+package com.example.rxjavalessonone.model;
+
+public class User {
+}
--- app/src/main/java/com/example/rxjavalessonone/utils/rx_utils/IRxUtils.java
+++ app/src/main/java/com/example/rxjavalessonone/utils/rx_utils/IRxUtils.java
@@ -1,4 +1,48 @@
 package com.example.rxjavalessonone.utils.rx_utils;
 
+import android.widget.EditText;
+
+import com.example.rxjavalessonone.model.User;
+import com.example.rxjavalessonone.model.UserEntity;
+
+import java.util.List;
+
+import rx.Observable;
+import rx.Observer;
+
 public interface IRxUtils {
+
+    Observable<String> introductionJustOperator(String string);
+
+    Observable<String> introductionErrorOperator(Throwable e);
+
+    Observable<Boolean> introductionMapOperator(String stringForMap);
+
+    Observable<List<UserEntity>> introductionFlatMapOperator(List<User> list);
+
+    Observable<List<UserEntity>> introductionSwitchMapOperator(EditText editText);
+
+    Observable<List<User>> introductionConcatMapOperator(List<User> list);
+
+    void introductionSubscribeOperator();
+
+    void introductionZipOperator(Observable<User> dataFromNetwork, Observable<UserEntity> dataFromDatabase);
+
+    void introductionMergeOperator(Observable<User> dataFromNetwork, Observable<User> dataFromDatabase);
+
+    void introductionCombineLatestOperator(EditText editTextLogin, EditText editTextPassword);
+
+    void introductionSubscribeOnWithObserveOnOperators(Observable observable);
+
+    void introductionPublishSubject();
+
+    void introductionReplaySubject();
+
+    void introductionBehaviorSubject();
+
+    void introductionAsyncSubject();
+
+    Observer<Integer> getFirsObserver();
+
+    Observer<Integer> getSecondObserver();
 }
